ccb72879935917e0cf2001f96f4a00c6e0d23061
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-28 23:59:52 +0200

    refactoring

--- compare/compare.go
+++ compare/compare.go
@@ -6,50 +6,45 @@ import (
 	"io"
 	"os"
 
-	"github.com/bborbe/log"
+	"runtime"
+
 	"github.com/bborbe/version"
+	"github.com/golang/glog"
 )
 
-var logger = log.DefaultLogger
-
 const (
-	PARAMETER_LOGLEVEL = "loglevel"
-	SUCCESS            = 0
-	FAILURE            = 1
+	SUCCESS = 0
+	FAILURE = 1
 )
 
 type Compare func(version.Version, version.Version) bool
 
 func Main(compare Compare) {
-	defer logger.Close()
-	logLevelPtr := flag.String(PARAMETER_LOGLEVEL, log.INFO_STRING, "one of OFF,TRACE,DEBUG,INFO,WARN,ERROR")
-
+	defer glog.Flush()
+	glog.CopyStandardLogTo("info")
 	flag.Parse()
-	logger.SetLevelThreshold(log.LogStringToLevel(*logLevelPtr))
-	logger.Debugf("set log level to %s", *logLevelPtr)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	writer := os.Stdout
 	returnCode, err := do(writer, compare, flag.Args())
 	if err != nil {
-		logger.Fatal(err)
-		returnCode = 1
+		glog.Exit(err)
 	}
-	logger.Close()
 	os.Exit(returnCode)
 }
 
 func do(writer io.Writer, compare Compare, versions []string) (int, error) {
 	if len(versions) != 2 {
-		logger.Debugf("arg count: %d", len(versions))
+		glog.V(2).Infof("arg count: %d", len(versions))
 		fmt.Fprintf(writer, "can only compare to versions\n")
-		logger.Debugf("arg count != 2 => failure")
+		glog.V(2).Infof("arg count != 2 => failure")
 		return FAILURE, nil
 	}
 	if compare(version.Version(versions[0]), version.Version(versions[1])) {
-		logger.Debugf("%v %v => succes", versions[0], versions[1])
+		glog.V(2).Infof("%v %v => succes", versions[0], versions[1])
 		return SUCCESS, nil
 	} else {
-		logger.Debugf("%v %v => failure", versions[0], versions[1])
+		glog.V(2).Infof("%v %v => failure", versions[0], versions[1])
 		return FAILURE, nil
 	}
 }
